cdc823c6f2690ad0eaa8f70087eeedff9c087995
shawnmarie <shawn.abbott@versionone.com>
2015-11-09 15:24:40 -0500

    updated jiraDelete with sync-request. script now works!

--- myJSONstudy/Test/jiraDelete.js
+++ myJSONstudy/Test/jiraDelete.js
@@ -1,40 +1,29 @@
-var request = require("request");
+var request = require("sync-request");
+
 var jiraProjId = "JST";
-//var epics = ["SMAS-509", "SMAS-510", "SMDEF-32", "SMDEF-31", "SMDEF-30", "SMSCRUM-55", "SMSCRUM-54", "SMSCRUM-53"];
 
 var issuesToDelete = [];
-var getIssues = { method: "GET",
-	url: 'http://jira-6.cloudapp.net:8080/rest/api/2/search?jql=project=' +jiraProjId + '&fields=summary',
+var res = request('GET','http://jira-6.cloudapp.net:8080/rest/api/2/search?jql=project=' +jiraProjId + '&fields=summary',{
 	headers:
 	  {'content-type': 'application/json',
-		authorization: 'Basic YWRtaW46YWRtaW4=' } };
-request(getIssues, function(error, response, body) {
-	if(error) throw new Error(error);
-	//console.log(body + " this is the body.");
-	var responseData = JSON.parse(body);
-	for(var i = 0; i < responseData.issues.length; i++){
-		issuesToDelete.push(responseData.issues[i].key);
-	}
-	//console.log(body);
-	console.log(issuesToDelete);
-	console.log("issues inside.");
-});
+		authorization: 'Basic YWRtaW46YWRtaW4=' } });
+
+var responseData = JSON.parse(res.getBody());
+console.log(responseData);
+for(var i = 0; i < responseData.issues.length; i++){
+	issuesToDelete.push(responseData.issues[i].key);
+}
+console.log(issuesToDelete);
+console.log("issues inside.");
 
 console.log(issuesToDelete);
 console.log("issues outside.");
 
 for(var i = 0; i < issuesToDelete.length; i++) {
 	var toDelete = issuesToDelete[i];
-	var options = { method: 'DELETE',
-	  url: 'http://jira-6.cloudapp.net:8080/rest/api/2/issue/' + toDelete,
+	var res = request('DELETE', 'http://jira-6.cloudapp.net:8080/rest/api/2/issue/' + toDelete, {
 	  headers: 
 	   { 'cache-control': 'no-cache',
 		 'content-type': 'application/json',
-		 authorization: 'Basic YWRtaW46YWRtaW4=' } };
-	console.log(options);
-	request(options, function (error, response, body) {
-	  if (error) throw new Error(error);
-
-	  console.log(body);
-	});
+		 authorization: 'Basic YWRtaW46YWRtaW4=' } });
 }

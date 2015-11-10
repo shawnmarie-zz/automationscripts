0d7634a3cf5301e2232df6922be364e8c9dfbc29
shawnmarie <shawn.abbott@versionone.com>
2015-11-10 12:31:32 -0500

    updated jiraDelete - removed commented lines

--- myJSONstudy/Test/jiraDelete.js
+++ myJSONstudy/Test/jiraDelete.js
@@ -9,15 +9,12 @@ var res = request('GET','http://jira-6.cloudapp.net:8080/rest/api/2/search?jql=p
 		authorization: 'Basic YWRtaW46YWRtaW4=' } });
 
 var responseData = JSON.parse(res.getBody());
-console.log(responseData);
+
 for(var i = 0; i < responseData.issues.length; i++){
 	issuesToDelete.push(responseData.issues[i].key);
 }
-console.log(issuesToDelete);
-console.log("issues inside.");
 
 console.log(issuesToDelete);
-console.log("issues outside.");
 
 for(var i = 0; i < issuesToDelete.length; i++) {
 	var toDelete = issuesToDelete[i];
